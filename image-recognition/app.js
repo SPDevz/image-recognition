83171777e4b090d46ad3d170e75209bda47c5ac2
SPDevz <peiquin@Peiquins-MacBook-Pro.local>
2019-01-15 20:30:45 +0800

    Use Heroku

--- image-recognition/app.js
+++ image-recognition/app.js
@@ -4,8 +4,6 @@ var app = express();
 
 app.get("/test", function(req, res){
     var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-    var fs = require('fs');
-
     var visualRecognition = new VisualRecognitionV3({
         version: '2018-03-19',
         iam_apikey: 'yBTV0Kd54On4MH7s8u4ecanbl_JT-mQ6NWyuKEpTZoNz'
@@ -17,17 +15,22 @@ app.get("/test", function(req, res){
         url:"https://www.t-mobile.com/content/dam/t-mobile/en-p/cell-phones/apple/apple-iphone-x/silver/Apple-iPhoneX-Silver-1-3x.jpg"
     };
 
+    //asynchronous javascript call
     visualRecognition.classify(params, function(err, response) {
         if (err)
-          console.log(err);
-        else
-          console.log(JSON.stringify(response, null, 2))
+            console.log(err);
+        else{
+            //Store response into a string
+            var result = JSON.stringify(response, null, 2)
+            res.end(result)
+            console.log(result)
+        }
     });
 
 })
 
-//var listener = app.listen(process.env.PORT,process.env.IP,function(){
-var listener = app.listen(4000,process.env.IP,function(){
+var listener = app.listen(process.env.PORT,process.env.IP,function(){
+//var listener = app.listen(4000,process.env.IP,function(){
     //var listener = app.listen(process.env.PORT,process.env.IP,function(){
     console.log("server has started");
     console.log('Listening on port ' + listener.address().port);
